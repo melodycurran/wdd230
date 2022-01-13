4a5c61d02304a9a8a993a746b2ca2eb42d4e151a
melodycurran <cur21014@byui.edu>
2022-01-13 14:35:52 -0700

    Added new page, folder, js and css

---
+++ lesson2/js/current-date-and-time.js
@@ -0,0 +1,6 @@
+
+    let d = new Date();
+
+    let newUpdate = document.getElementById('footer-box');
+    newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
+    newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
