662e96ec065a74779d57fd6e65f87c0dfd6834a5
melodycurran <cur21014@byui.edu>
2022-02-05 16:31:36 -0700

    added more designs and scripts

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -17,7 +17,7 @@ document.querySelector('#now').innerHTML = new Intl.DateTimeFormat("en-US", { da
 document.querySelector('#year').innerHTML = new Date().getFullYear();
 document.querySelector('#currentDateandTime').innerHTML = document.lastModified;
 
-
+// Script for the weather widget
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
 
 if(!d.getElementById(id)){js=d.createElement(s);
@@ -25,3 +25,24 @@ if(!d.getElementById(id)){js=d.createElement(s);
     fjs.parentNode.insertBefore(js,fjs);
 }}
 (document,'script','weatherwidget-io-js');
+
+// Script for the to display a banner on Mondays or Tuesdays only at the very 
+// top of the page that says "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m." 
+const banner = document.querySelector('#top-page-banner');
+
+let dayOfWeek = new Date().getDay();
+
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    banner.style.display = 'block';
+}
+else {
+    banner.style.display = 'none';
+}
+
+// Script for closing the banner
+const closeBtn = document.querySelector('.bannerBtn');
+closeBtn.addEventListener('click', function() {
+    if (banner.style.display !== "none") {
+        banner.style.display = "none";
+    }
+})
