cae6b9d62cf6a52a756785c4067890a2dd6d3823
melodycurran <cur21014@byui.edu>
2022-02-05 00:40:03 -0700

    added more details to chamber homepage

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -16,3 +16,12 @@ document.querySelector('#now').innerHTML = new Intl.DateTimeFormat("en-US", { da
 // script for footer year and date modified
 document.querySelector('#year').innerHTML = new Date().getFullYear();
 document.querySelector('#currentDateandTime').innerHTML = document.lastModified;
+
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+
+if(!d.getElementById(id)){js=d.createElement(s);
+    js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+    fjs.parentNode.insertBefore(js,fjs);
+}}
+(document,'script','weatherwidget-io-js');
