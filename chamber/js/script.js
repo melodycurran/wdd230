2c1aeeb12167a735c4bbe269ec4cdf40915e482f
melodycurran <cur21014@byui.edu>
2022-02-25 14:39:25 -0700

    added join page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -42,3 +42,11 @@ closeBtn.addEventListener('click', function() {
 })
 
 
+// Script for Validating the RegEx
+let input = document.querySelector('#businessPosition');
+input.oninvalid = function(event) {
+    event.target.setCustomValidity('Business Position should only contain alpha characters, hyphen, space and up to 7 characters only.');
+}
+
+
+
