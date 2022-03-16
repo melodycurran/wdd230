42dccdd1e97592266b3ca42e01188c3a680321a3
melodycurran <cur21014@byui.edu>
2022-03-16 17:13:18 -0600

    replaced json data

--- chamber/js/spotlights.js
+++ chamber/js/spotlights.js
@@ -4,27 +4,29 @@ dataURL = 'https://melodycurran.github.io/wdd230/chamber/js/data.json';
 spotlight1Div = document.querySelector('.spotlight1');
 spotlight2Div = document.querySelector('.spotlight2');
 
-
 fetch(dataURL)
 .then((response) => response.json())
 .then((jsonObject) => {
     console.log(jsonObject);
 
-    
+    number  = 0;
+    random = Math.floor(Math.random() * number++);
 
-    if (jsonObject.businesses[0]) {
-        get_content(`FREE Protein Shake Apr. 1 - May 15`, 'images/free-protein-shake.webp', 'free-protein-shake', jsonObject.businesses[0], spotlight1Div);
-    } if (jsonObject.businesses[8]) {
-        get_content(`On-Demand Bus Until Spring`, 'images/on-demand-bus.webp', 'on-demand-bus', jsonObject.businesses[8], spotlight1Div);
-    } if (jsonObject.businesses[3]) {
-        get_content(`Tree Planting Initiatives`, 'images/tree-planting.webp', 'tree-planting-initiatives', jsonObject.businesses[3], spotlight1Div);
-    } if (jsonObject.businesses[11]) {
-        get_content(`Toys for Tots Gift-giving`, 'images/toys-for-tots.webp', 'toys-for-tots', jsonObject.businesses[11], spotlight2Div);
-    }  if (jsonObject.businesses[4]) {
-        get_content(`Spring Harvest Parade`, 'images/spring-harvest-parade.webp', 'spring-harvest-parade', jsonObject.businesses[4], spotlight2Div);
-    }  if (jsonObject.businesses[9]) {
-        get_content(`Art In The Park Registration Open`, 'images/art-in-the-park.webp', 'art-in-the-park', jsonObject.businesses[9], spotlight2Div);
+    if (jsonObject.businesses[random]) {
+        get_content(`FREE Protein Shake Apr. 1 - May 15`, 'images/free-protein-shake.webp', 'free-protein-shake', jsonObject.businesses[random], spotlight1Div);
+    } if (jsonObject.businesses[random]) {
+        get_content(`On-Demand Bus Until Spring`, 'images/on-demand-bus.webp', 'on-demand-bus', jsonObject.businesses[random], spotlight1Div);
+    } if (jsonObject.businesses[random]) {
+        get_content(`Tree Planting Initiatives`, 'images/tree-planting.webp', 'tree-planting-initiatives', jsonObject.businesses[random], spotlight1Div);
+    } if (jsonObject.businesses[random]) {
+        get_content(`Toys for Tots Gift-giving`, 'images/toys-for-tots.webp', 'toys-for-tots', jsonObject.businesses[random], spotlight2Div);
+    } if (jsonObject.businesses[random]) {
+        get_content(`Spring Harvest Parade`, 'images/spring-harvest-parade.webp', 'spring-harvest-parade', jsonObject.businesses[random], spotlight2Div);
+    } if (jsonObject.businesses[random]) {
+        get_content(`Art In The Park Registration Open`, 'images/art-in-the-park.webp', 'art-in-the-park', jsonObject.businesses[random], spotlight2Div);
     }
+    
+    
 
 
 
@@ -53,6 +55,9 @@ fetch(dataURL)
         div.appendChild(container);
         
     }
+
 });
+
+
 
     
